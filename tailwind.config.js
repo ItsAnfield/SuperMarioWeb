4a34ab9961e6b2e225645091885623288310ec12
Toni Minguez <toniminguez1@gmail.com>
2022-06-01 22:00:46 +0200

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './public/**/*.html',
+    ],
+    darkMode: false,
+    theme: {
+        extend:  {}
+    },
+    variants: {
+        extend:  {},
+    },
+    plugins: [],
+}
